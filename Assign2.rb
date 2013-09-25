8b768e22099c517e845620270a3f461fa67e046c
Chris Fichman <christopher.fichman@colorado.edu>
2013-09-25 13:08:41 -0600

    Updated Assign2

--- Assign2.rb
+++ Assign2.rb
@@ -1,7 +1,39 @@
 #Name: Chris Fichman
 #Program: Assign2.rb
 #Email: chris.fichman@gmail.com
-#Last Edit: 9/17/2013
+#Last Edit: 9/24/2013
+#Collaborator: Tony Gagliardi
+
+# PART 0: RPS Tourney
+class WrongNumberOfPlayersError < StandardError; end
+class NoSuchStrategyError < StandardError; end
+
+def rps_game_winner(game)
+	raise WrongNumberOfPlayersError unless game.length == 2
+	viable = ["P","S","R"]
+	#Help from Tony on this if statment
+	if (viable.include?(game[0][1].upcase) && viable.include?(game[1][1].upcase))
+		if (game[0][1] + game[1][1] =~ /SS|PP|RR|SP|RS|PR/i)
+			game[0]
+		else
+			game[1]
+		end
+	else
+		raise NoSuchStrategyError
+	end
+end	
+
+def rps_tournament(tourney)
+	if tourney[0][1].is_a? String
+		rps_game_winner(tourney)
+	else
+		rps_game_winner([rps_tournament(tourney[0]), 
+					rps_tournament(tourney[1])])
+	end
+end
+
+input = [ [ [ ["Armando", "P"], ["Dave", "S"] ], [ ["Richard", "R"],  ["Michael", "S"] ],],[ [ ["Allen", "S"], ["Omer", "P"] ], [ ["David E.", "R"], ["Richard X.", "P"] ] ] ]
+puts rps_tournament(input)
 
 # PART I: Classes
 
@@ -43,17 +75,19 @@ class JellyBean < Dessert
 		end
 	end
 end
+  
+# Tests
 
 b1 = JellyBean.new("Bean", 4, "black licorice")
 b2 = JellyBean.new("Bean", 4, "Strawberry")
 
-puts "Is b1 healthy?"
+puts "Is a black licorice healthy?"
 puts b1.healthy?.to_s
-puts "Is b1 tasty?"
+puts "Is black licorice tasty?"
 puts b1.delicious?.to_s
-puts "Is b2 healthy?"
+puts "Is strawberry healthy?"
 puts b2.healthy?.to_s
-puts "Is b2 tasty?"
+puts "Is strawberry tasty?"
 puts b2.delicious?.to_s
 
 # PART II: Object Oriented Programming
@@ -79,7 +113,91 @@ class Foo
 	attr_accessor_with_history :bar
 end
 
+# Test
 f = Foo.new
 f.bar = 1
 f.bar = 2
 puts f.bar_history.to_s
+
+# PART III: More OOP
+module Currency
+    @@currencies = {'dollar' => 1.0, 'yen' => 0.013, 'euro' => 1.292, 'rupee' => 0.019}
+    attr_accessor :currency
+    
+    def method_missing(method_id, *args, &block)
+        method_id = method_id.to_s.gsub( /s$/, '')
+        if @@currencies.has_key?(method_id)
+            @currency = method_id
+            self
+        else
+            super
+        end
+    end
+    
+
+    def in(tar_currency)
+		tar_currency = tar_currency.to_s.gsub( /s$/, '')
+		result = (self *  @@currencies[@currency][tar_currency]).round(2)
+		
+		result.currency = tar_currency
+		result
+    end
+end
+class Numeric
+	include Currency
+end
+# Test
+#puts (1000.yen.in(:dollar))
+#puts (492.rupees.in(:euro))
+#puts (1000.dollar.in(:rupees))
+
+# Updated Palindrome
+class String
+    def palindrome?()
+		string = (self.gsub(/[^a-zA-Z]/, "")).downcase
+		rstring = string.reverse
+		
+		return (string == rstring)
+    end
+end
+
+puts ("RaCeCaR rAcEcAr".palindrome?)
+puts ("Hingadingadurgen".palindrome?)
+
+# Enumerated Palindrome
+module Enumerable
+    def palindrome?()
+        string = self
+        pal = []
+        # iterates over every element of the object pal
+        string.reverse_each {|i| pal << i}
+        if(string == pal)
+            true
+        else
+            false
+        end
+    end
+end
+
+puts ([1, 2, 3, 2, 1].palindrome?)
+
+# PART IV: Blocks
+
+class CartesianProduct
+    include Enumerable
+    def initialize(cart1, cart2)
+        @cart1 = cart1
+        @cart2 = cart2
+    end
+    def each
+        @cart1.each do |i|
+            @cart2.each do |j|
+                yield ([i, j])
+            end
+        end
+    end
+end
+
+# Test
+test = CartesianProduct.new([:a, :b], [4, 5])
+test.each { |elt| puts elt.inspect}
